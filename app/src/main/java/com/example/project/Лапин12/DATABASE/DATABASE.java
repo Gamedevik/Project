7c4fe95dc2a0f9e5476f23fa95ca2c654e2b5790
Gamedevik <pcelovek102@gmail.com>
2026-04-23 15:33:06 +0400

    Delete app/src/main/java/com/example/project/Лапин12/DATABASE/DATABASE.java

--- app/src/main/java/com/example/project/Лапин12/DATABASE/DATABASE.java
+++
@@ -1,6 +0,0 @@
-package com.example.project.Лапин12.DATABASE;
-
-import android.database.sqlite.SQLiteDatabase;
-import android.view.Display;
-
-SQLiteDatabase db = getBaseContext().openOrCreateDatabase("foodcat", Mode, null);
